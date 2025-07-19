10d4c693221d4505576d7dc075176f6c525a0067
ShubhoshishRoy08 <shubho0208@gmail.com>
2025-07-19 13:49:26 +0530

    Echo_Application

--- backend/socket_prog_java/EchoClient.java
+++ backend/socket_prog_java/EchoClient.java
@@ -1,14 +1,25 @@
 package socket_prog_java;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.Buffer;
+// import java.nio.Buffer;
 public class EchoClient {
     public static void main(String[] args) {
         try {
             System.out.println("Connecting to server...");
-            Socket socket = new Socket("localhost", 9806);
-            System.out.println("Connected to server: " + socket.getInetAddress().getHostAddress());
-            System.out.println("Connection established");
+            Socket soc = new Socket("localhost", 9806);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter a String");
+            String str = userInput.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println(str);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
+        
     }
 }
--- backend/socket_prog_java/EchoServer.java
+++ backend/socket_prog_java/EchoServer.java
@@ -1,14 +1,19 @@
 package socket_prog_java;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 public class EchoServer {
     public static void main(String[] args) {
         try {
             System.out.println("Waiting for clients.....");
             ServerSocket ss = new ServerSocket(9806);
             Socket soc = ss.accept();
-            System.out.println("Client connected: " + soc.getInetAddress().getHostAddress());
             System.out.println("Connection established");
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String str = in.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            out.println("Server says: "+str);
+
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
